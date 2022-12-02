abdb5cf9966235ed68662096e673ffe1f438f441
safesalmon <119700066+safesalmon@users.noreply.github.com>
2022-12-02 11:28:18 -0800

    Created first file in the sim-crab game
    comments/instructions for coding

---
+++ crab_game.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 11:19:54 2022
+
+@author: safesalmon
+"""
+
+"""
+Instructions:
+Copy your notes into function-style code stubs like this:
+    
+def run_crab:
+    # crab should run away when a predator is near by
+    pass     # <- lets the code still work
+
+"""
+
+
